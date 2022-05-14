649dd947145626077338795f61b35301981f5982
MadoxMC <89165041+xodamcm@users.noreply.github.com>
2022-05-14 03:54:04 +0200

    Delete Fireproof

--- src/tgwaste/NetherMobs/Attributes.php
+++ src/tgwaste/NetherMobs/Attributes.php
@@ -23,11 +23,11 @@ class Attributes {
 		return in_array($name, ["Blaze", "Ghast", "Enderman", "Ghast", "MagmaCube", "Skeleton", "WitherSkeleton", "Strider", "Piglin", "Hoglin", "ZombiePigman1", "ZombiePigman2", "ZombiePigman3", "ZombiePigman4"]);
 	}
 	
-	public function isFireProof(string $name) : bool {
+	/**public function isFireProof(string $name) : bool {
 		return in_array($name, [
 			"Blaze", "Ghast", "MagmaCube", "ZombiePigman", "WitherSkeleton", "Piglin", "Strider", "Hoglin", "ZombiePigman1", "ZombiePigman2", "ZombiePigman3", "ZombiePigman4"
 		]);
-	}
+	}*/
 	
 	public function getMortalEnemy(string $name) : string {
 		$enemies = array();
