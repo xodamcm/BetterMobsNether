d918d08d71999974525cc255ca8a06cfd922febb
MadoxMC <89165041+xodamcm@users.noreply.github.com>
2022-05-19 21:44:26 +0200

    Add MagmaCube Different Size

--- src/tgwaste/NetherMobs/Entities/MagmaCube.php
+++ src/tgwaste/NetherMobs/Entities/MagmaCube.php
@@ -6,10 +6,12 @@ namespace tgwaste\NetherMobs\Entities;
 use pocketmine\entity\Living;
 use pocketmine\item\Item;
 use pocketmine\player\Player;
+use function mt_rand;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataCollection;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\item\enchantment\Enchantment;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
-use function mt_rand;
 use pocketmine\entity\EntitySizeInfo;
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 use pocketmine\item\VanillaItems;
@@ -20,7 +22,8 @@ class MagmaCube extends MobsEntity {
 	const HEIGHT = 1.0;
     public function initEntity(CompoundTag $nbt) : void{
         $this->setMaxHealth(10);
-	 $this->setMovementSpeed(1);
+	$this->setMovementSpeed(1);
+        $this->setVariant(mt_rand(0, 3));
         parent::initEntity($nbt);
     }
     public function getDrops(): array
@@ -46,5 +49,19 @@ class MagmaCube extends MobsEntity {
     {
         return mt_rand(1, 4);
     }
+
+    
+    public function setVariant(int $variant = 0): void {
+		if($variant > 2 && $variant < 0){
+			$variant = 0;
+		}
+		$this->variant = $variant;
+		$this->networkPropertiesDirty = true;
+	} 
+	
+    protected function syncNetworkData(EntityMetadataCollection $properties) : void{
+		parent::syncNetworkData($properties);
+		$properties->setInt(EntityMetadataProperties::VARIANT, $this->variant);
+	}
 
 }
